93db91b65a672c7e32607e52ddc8edf464e49d74
taogen <taogenjia@gmail.com>
2019-04-06 18:32:23 +0800

    ADD. gitignore and algorithms folder

---
+++ algorithms/dynamicProgramming/fibonaciiNumber/fibonaciiNumber_recursion.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int getFibonaciiNumber(int n);
+int main() {
+	int n = 1;
+	std::cin >> n;
+	std::cout << getFibonaciiNumber(n);
+	return 0;
+}
+
+int getFibonaciiNumber(int n) {
+	if (n == 1 || n == 2)
+	{
+		return 1;
+	}
+	return getFibonaciiNumber(n - 1) + getFibonaciiNumber(n - 2);
+}
