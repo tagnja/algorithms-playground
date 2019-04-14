4487ae8e39abeef0ff4f4d3f2fea4c465f92acbc
taogen <taogenjia@gmail.com>
2019-04-14 21:57:16 +0800

    ADD. stack and queue implementation

---
+++ data-structures/Queue/Queue.java
@@ -0,0 +1,87 @@
+package com.taogen.ds;
+
+/**
+ * Queue
+ *     int front;
+ *     int rear;
+ *     int size;
+ *     int a[];
+ *     isEmpty();
+ *     isFull();
+ *     size();
+ *     enqueue();
+ *     dequeue();
+ *     peek();
+ */
+public class Queue 
+{
+	int front, rear, size, capacity, a[];
+	public Queue(int capacity)
+	{
+		this.capacity = capacity;
+		front = size = 0;
+		rear = capacity - 1;
+		a = new int[capacity];
+	}
+	
+	boolean isEmpty()
+	{
+		return (size == 0);
+	}
+	
+	boolean isFull()
+	{
+		return (size == capacity);
+	}
+	
+	int size()
+	{
+		return size;
+	}
+	
+	void enqueue(int data)
+	{
+		if (isFull())
+		{
+			return;
+		}
+		rear = (rear + 1) % capacity;
+		a[rear] = data;
+		size++;
+	}
+	
+	int dequeue()
+	{
+		if (isEmpty())
+		{
+			return -1;
+		}
+		int item = a[front];
+		front = (front + 1) % capacity;
+		size--;
+		return item;
+	}
+	
+	public static void main(String[] args) 
+	{
+		Queue queue = new Queue(3);
+		queue.enqueue(1);
+		System.out.println("size: " + queue.size);
+		queue.enqueue(2);
+		System.out.println("size: " + queue.size);
+		queue.dequeue();
+		System.out.println("size: " + queue.size);
+		queue.enqueue(3);
+		System.out.println("size: " + queue.size);
+		queue.enqueue(4);
+		System.out.println("isFull: "+ queue.isFull());
+		System.out.println("size: " + queue.size);
+		
+		System.out.println("print queue: ");
+		while (! queue.isEmpty())
+		{
+			System.out.println(queue.dequeue());
+		}
+		System.out.println("isEmpty: "+ queue.isEmpty());
+	}
+}
---
+++ data-structures/Stack/Stack.java
@@ -0,0 +1,107 @@
+package com.taogen.ds;
+
+/**
+ * Stack
+ *     int MAX;
+ *     int top;
+ *     int a[MAX];
+ *     isEmpty();
+ *     isFull();
+ *     size();
+ *     push();
+ *     pop();
+ *     peek();
+ *
+ */
+public class Stack 
+{
+	static final int MAX = 3;
+	int top;
+	int a[] = new int[MAX];
+	
+	public Stack()
+	{
+		top = -1;
+	}
+	
+	boolean isEmpty()
+	{
+		return (top < 0);
+	}
+	
+	boolean isFull()
+	{
+		return (top == MAX - 1);
+	}
+	
+	int size()
+	{
+		return top + 1;
+	}
+	
+	boolean push(int data)
+	{
+		if (top + 1 > MAX - 1)
+		{
+			System.out.println("Stack overflow!");
+			return false;
+		}
+		else
+		{
+			a[++top] = data;
+			return true;
+		}
+	}
+	
+	int pop()
+	{
+		if (top < 0)
+		{
+			System.out.println("Stack underflow!");
+			return -1;
+		}
+		else
+		{
+			return a[top--];
+		}
+	}
+	
+	int peek()
+	{
+		if (top < 0)
+		{
+			System.out.println("Stack is empty!");
+			return -1;
+		}
+		else
+		{
+			return a[top];
+		}
+	}
+	
+	public static void main(String[] args) 
+	{
+		Stack stack = new Stack();
+		stack.push(1);
+		System.out.println("top: " + stack.top);
+		stack.push(2);
+		System.out.println("top: " + stack.top);
+		stack.push(3);
+		System.out.println("size: " + stack.size());
+		System.out.println("peek: " + stack.peek());
+		System.out.println("isFull: " + stack.isFull());
+		System.out.println("stack pop: " + stack.pop());
+		System.out.println("isFull: " + stack.isFull());
+		stack.push(4);
+		System.out.println("top: " + stack.top);
+		stack.push(5);
+		System.out.println("top: " + stack.top);
+		
+		System.out.println("Print stack: ");
+		while (! stack.isEmpty())
+		{
+			System.out.println(stack.pop());
+		}
+		System.out.println("top: " + stack.top);
+	}
+}
